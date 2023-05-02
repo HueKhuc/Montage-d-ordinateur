417991d3b8ec29a9fe6e878f3cab64dfcb64bd56
Sami Abdelkafi <s.abdelkafi.pro@gmail.com>
2023-05-02 11:36:14 +0200

    Ticket #3 - Création Footer

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./Styles/bootstrap.css">
+  <link rel="stylesheet" href="./Styles/style.css">
+</head>
+
+<body>
+  <div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <p class="col-md-4 mb-0 text-body-secondary">PMO © 2023</p>
+      <a href="/"
+        class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32">
+          <use xlink:href="#bootstrap"></use>
+        </svg>
+      </a>
+      <ul class="nav col-md-4 justify-content-end">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Accueil</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Liste de pièces</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Liste de modèles</a></li>
+      </ul>
+    </footer>
+  </div>
+</body>
+
+</html>
