302245f187aad594bb214b4e4c7c78ce2dccf4eb
HueKhuc <khuchue.hus@gmail.com>
2023-05-02 11:21:00 +0200

    2- Creation header

---
+++ includes/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./Styles/bootstrap.css">
+    <link rel="stylesheet" href="./Styles/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-dark fixed-top navbar-expand-lg bg-dark" data-bs-theme="dark">
+                <div class="container-fluid">
+                <a class="navbar-brand" href="#">Montage d'ordinateur</a>
+                    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Liste des modèles</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Liste des pièces</a>
+                            </li>
+                        </ul>
+                        <div class="navbar-nav">
+                                <a class="nav-link active nav-item" href="#">Connexion</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
