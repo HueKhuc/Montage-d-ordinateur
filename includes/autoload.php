17bfd35d6302f8e22e9eccb356e9ab19fd504091
David Chosson <davidcho868@gmail.com>
2023-05-04 11:44:07 +0200

    reparation autoload

--- includes/autoload.php
+++ includes/autoload.php
@@ -1,8 +1,8 @@
-spl_autoload_register(function ($class) {
+<?php spl_autoload_register(function ($class) {
     require_once './classes/' . $class . '.php';
 });
 spl_autoload_register(function ($class) {
     require_once './classes/Factory/' . $class . '.php';
 });
-require_once 'fonction.php';
-require_once 'config.inc.php';
+require_once 'includes/function.php';
+require_once 'includes/config.inc.php';
