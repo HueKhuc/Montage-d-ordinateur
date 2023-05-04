49831ffb787409203860ed124f578ba9d957cada
David Chosson <davidcho868@gmail.com>
2023-05-04 10:22:48 +0200

    Creation des navbar utilisateur

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,7 @@ session_start();
 require_once 'autoload.php';
 require_once 'config.inc.php';
 
+
 ?>
 
 
@@ -23,71 +24,100 @@ require_once 'config.inc.php';
 
 <body>
     <header>
-        <div class="container">
-            <nav class="navbar navbar-dark fixed-top navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <nav class="navbar navbar-dark navbar-expand-lg bg-dark" data-bs-theme="dark">
                 <div class="container-fluid">
-                <a class="navbar-brand" href="#">Montage d'ordinateur</a>
+                    <a class="navbar-brand" href="#">
+                    <?php if (isset($_SESSION['type']) && $_SESSION['type'] == 'monteur') {
+                        echo "Montage d'ordinateur"; 
+                    } elseif (isset($_SESSION['type']) && $_SESSION['type'] == 'concepteur') { 
+                        echo "Conception d'ordinateur";
+                    } else { 
+                        echo "PMO"; 
+                    } ?>
+                    </a>
                     <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link active" aria-current="page" href="#">
+                                    <?php if (isset($_SESSION['type']) && $_SESSION['type'] == 'monteur') {
+                        echo "Liste des modèles"; 
+                    } elseif (isset($_SESSION['type']) && $_SESSION['type'] == 'concepteur') { 
+                        echo "Statistiques";
+                    }  ?> </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" href="#">Liste des modèles</a>
+                                <a class="nav-link active" href="#">
+                                    <?php if (isset($_SESSION['type']) && $_SESSION['type'] == 'monteur') {
+                        echo "Liste des pièces"; 
+                    } elseif (isset($_SESSION['type']) && $_SESSION['type'] == 'concepteur') { 
+                        echo "Ajout pièces";
+                    }?>
+                                </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" href="#">Liste des pièces</a>
+                                <a class="nav-link active" href="#">
+                                    <?php if (isset($_SESSION['type']) && $_SESSION['type'] == 'concepteur') { 
+                        echo "Créa/Modif modèle";
+                    }?>
+                                </a>
                             </li>
                             <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="?page=commun/inscription">S'inscrire</a>
-        </li>
-        <?php
-        if (isset($_SESSION['nom'])) {
-        ?>
-        <li class="nav-item">
-        <a class="nav-link" href="?page=commun/login"><?php echo $_SESSION['nom']; ?></a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="?page=commun/logout">Deconnexion</a>
-        </li>
-        
-        <?php
-    } else {
-    ?>
-    <li class="nav-item">
-        <a class="nav-link" href="?page=commun/login">Connexion</a>
-        </li>
-        <?php
-    }?>
-    </ul>
+                                <a class="nav-link active" href="#">
+                                    <?php if (isset($_SESSION['type']) && $_SESSION['type'] == 'monteur') {
+                        echo "Commentaires "; 
+                    } elseif (isset($_SESSION['type']) && $_SESSION['type'] == 'concepteur') { 
+                        echo "Stock";
+                    }  ?>
+                                </a>
+                            </li>
+                            <?php
+                            if (isset($_SESSION['nom'])) {
+                                ?>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="?page=commun/login">
+                                        <?php echo $_SESSION['nom']; ?>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="?page=commun/logout">Deconnexion</a>
+                                </li>
 
-    </div>
-</div>
-</nav>
-<?php
-if (isset($_GET['login']) && $_GET['login'] == "success") {
-?>
-<div class="alert alert-success" role="alert">
-  Vous êtes connecté ! 
-</div>
-<?php
-}
+                                <?php
+                            } else {
+                                ?>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="?page=commun/login">Connexion</a>
+                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page"
+                                    href="?page=commun/inscription">S'inscrire</a>
+                            </li>
+                                <?php
 
-
-if(isset($_GET['logout']) && $_GET['logout'] == "success") {
-  
-?>
-<div class="alert alert-success" role="alert">
-  Vous êtes deconnecté ! 
-</div>
-
-<?php
-}
-?>
+                            } ?>
                         </ul>
-                        
+
                     </div>
                 </div>
             </nav>
-        </div>
+        <?php
+        if (isset($_GET['login']) && $_GET['login'] == "success") {
+            ?>
+            <div class="alert alert-success" role="alert">
+                Vous êtes connecté !
+            </div>
+            <?php
+        }
+
+
+        if (isset($_GET['logout']) && $_GET['logout'] == "success") {
+            ?>
+            <div class="alert alert-success" role="alert">
+                Vous êtes deconnecté !
+            </div>
+
+            <?php
+
+        }
+        ?>
     </header>
