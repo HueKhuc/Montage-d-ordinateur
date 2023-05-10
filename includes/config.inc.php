c16e04d86dd6abb170cbb3031ccce78a88048e42
Sami Abdelkafi <s.abdelkafi.pro@gmail.com>
2023-05-10 14:38:14 +0200

    Footer ok

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -2,12 +2,9 @@
 $dsn = 'mysql:dbname=montage_ordi;port=3306;host=127.0.0.1';
 $user = 'root'; // Utilisateur par défaut
 $password = ''; // Par défaut, pas de mot de passe sur Wamp
-
 try {
     $db = new PDO($dsn, $user, $password, [
-
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]);
 } catch (PDOException $e) {
