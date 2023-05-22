9fce7a0d2f098eaa66763a93fefa0156b1c1bf71
Sami Abdelkafi <s.abdelkafi.pro@gmail.com>
2023-05-22 15:54:18 +0200

    Début structure MVC

---
+++ src/Controller/Commun/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+namespace Controller\Commun;
+use Controller\AbstractController;
+
+class HomeController extends AbstractController {
+
+public function getContent(): array {
+  return [];
+}
+
+public function getFileName(): string {
+  return 'Commun/home';
+}
+
+public function getPageTitle(): string {
+  return 'Accueil';
+}
+
+}
+?>
